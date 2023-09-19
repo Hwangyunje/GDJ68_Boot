a97f1245a9ef54bf179e1921c4ad7c9430eda60a
hwang <dnjy1945@naver.com>
2023-09-19 09:15:46 +0900

    0915

--- src/main/java/com/winter/app/board/notice/NoticeDAO.java
+++ src/main/java/com/winter/app/board/notice/NoticeDAO.java
@@ -1,8 +1,6 @@
 package com.winter.app.board.notice;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
-
 import com.winter.app.board.BoardDAO;
 
 //@Repository
