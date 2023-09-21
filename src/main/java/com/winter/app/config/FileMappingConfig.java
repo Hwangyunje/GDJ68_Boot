25d69dfa34591d1b5e599fe3b256532cc2fc31c1
hwang <dnjy1945@naver.com>
2023-09-21 12:46:22 +0900

    0921

--- src/main/java/com/winter/app/config/FileMappingConfig.java
+++ src/main/java/com/winter/app/config/FileMappingConfig.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//***-context.xml과 같은 역할
+//***-context.xml 과 같은 역할
+
 @Configuration
 public class FileMappingConfig implements WebMvcConfigurer {
 	
@@ -13,17 +14,19 @@ public class FileMappingConfig implements WebMvcConfigurer {
 	@Value("${app.upload.mapping}")
 	private String filePath;
 	
-	//요청 url 경로
+	//요청 URL 경로
 	@Value("${app.url.path}")
 	private String urlPath;
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		//요청url
-		registry.addResourceHandler(urlPath);
-		//local file 위치 file:///D:GDJ68/upload/
-//		registry.addResourceLocations(filePath);
+		
+		//요청 URL  /files/**
+		registry.addResourceHandler(urlPath)
+		//Local file 위치 file:///D:GDJ68/upload/
+				.addResourceLocations(filePath);
 		
 	}
 	
+
 }
