a6b69cb1b29d41169b1ba6eeb804e869a2e0335d
hwang <dnjy1945@naver.com>
2023-09-25 17:39:41 +0900

    0925

--- src/main/java/com/winter/app/member/MemberController.java
+++ src/main/java/com/winter/app/member/MemberController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,41 +24,34 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/member/*")
 @Slf4j
 public class MemberController {
+	
+	//service
 	@Autowired
 	private MemberService memberService;
-//	@GetMapping("join")
-//	public void setJoin(Model model) throws Exception{
-//		MemberVO memberVO = new MemberVO();
-//		model.addAttribute("memberVO",memberVO);
-//		
-//	}
+	
 	@GetMapping("update")
-	public void setUpdate(HttpSession session,Model model)throws Exception{
+	public void setUpdate(HttpSession session, Model model)throws Exception{
 		MemberVO memberVO = (MemberVO)session.getAttribute("member");
-		memberVO =memberService.getLogin(memberVO);
+		//memberVO = memberService.getLogin(memberVO);
 		
-		MemberInfoVO memberInfoVO=new MemberInfoVO();
+		MemberInfoVO memberInfoVO = new MemberInfoVO();
 		memberInfoVO.setName(memberVO.getName());
 		memberInfoVO.setBirth(memberVO.getBirth());
 		memberInfoVO.setEmail(memberVO.getEmail());
 		
 		
 		
-		model.addAttribute("memberVO",memberVO);
+		model.addAttribute("memberInfoVO", memberInfoVO);
 	}
+	
 	@PostMapping("update")
-	public void setUpdate(@Valid MemberInfoVO memberInfoVO,BindingResult bindingResult)throws Exception{
+	public void setUpdate(@Valid MemberInfoVO memberInfoVO, BindingResult bindingResult)throws Exception{
 		
-		
-		}
 	
-	
-	
-	
+	}
 	
 	@GetMapping("logout")
-	public String getLogout(HttpSession session)throws Exception{
-		//세션시간0으로
+	public String getLogout(HttpSession session) throws Exception {
 		session.invalidate();
 		
 		return "redirect:../";
@@ -65,38 +59,36 @@ public class MemberController {
 	
 	@GetMapping("login")
 	public void getLogin(@ModelAttribute MemberVO memberVO)throws Exception{
+		
+	}
+	
 
-	
-	
-	}
-	@PostMapping("login")
-	public String getLogin(MemberVO memberVO,HttpSession session)throws Exception{
-		memberVO=memberService.getLogin(memberVO);
-		
-		if(memberVO !=null) {
-			session.setAttribute("member", memberVO);
-			return "redirect:../";
-		}
-		
-		return "redirect:./login";
-		
-	}
-	
+//	@GetMapping("join")
+//	public void setJoin(Model model)throws Exception{
+//		MemberVO memberVO = new MemberVO();
+//		model.addAttribute("memberVO", memberVO);
+//	}
 	@GetMapping("join")
-	public void setJoin(@ModelAttribute MemberVO memberVO) throws Exception{
+	public void setJoin(@ModelAttribute MemberVO memberVO)throws Exception{
 		
 	}
+	
 	@PostMapping("join")
-	public String setJoin(@Valid MemberVO memberVO,BindingResult bindingResult, MultipartFile photo) throws Exception{
+	public String setJoin(@Valid MemberVO memberVO,BindingResult bindingResult, MultipartFile photo)throws Exception{
 		
-		boolean check=memberService.getMemberError(memberVO, bindingResult);
+		 boolean check = memberService.getMemberError(memberVO, bindingResult);
 		
-		if(bindingResult.hasErrors()|| check) {
+		
+		if(bindingResult.hasErrors() || check) {
 			return "member/join"; 
 		}
-		//회원가입 진행
 		
-		log.info("Photo : {}---- size: {}", photo.getOriginalFilename(),photo.getSize());
+		int result = memberService.setJoin(memberVO);
+		
+		
+		log.info("Photo : {} --- size : {}", photo.getOriginalFilename(), photo.getSize());
 		return "redirect:../";
 	}
+	
+
 }
--- src/main/java/com/winter/app/member/MemberService.java
+++ src/main/java/com/winter/app/member/MemberService.java
@@ -1,50 +1,78 @@
 package com.winter.app.member;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 
-@Service
-public class MemberService {
+import lombok.extern.slf4j.Slf4j;
 
+@Service
+@Slf4j
+public class MemberService implements UserDetailsService {
+	
 	//DAO
 	@Autowired
 	private MemberDAO memberDAO;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
-	//login
-	public MemberVO getLogin(MemberVO memberVO)throws Exception{
-		MemberVO loginVO=memberDAO.getMember(memberVO);
-		
-		if(loginVO==null) {
-			return loginVO;
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		log.info("====== 로그인 시도 중 ==========");
+		MemberVO memberVO = new MemberVO();
+		memberVO.setUsername(username);
+		try {
+			memberVO = memberDAO.getMember(memberVO);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			memberVO=null;
 		}
-		
-		if(loginVO.getPassword().equals(memberVO.getPassword())){
-			return loginVO;
-		}
-		
-		return null;
+		return memberVO;
 	}
 	
+
 	//검증메서드
-	public boolean getMemberError(MemberVO memberVO, BindingResult bindingResult)throws Exception {
-		boolean check=false;//error가 없다, true:error가있다. 검증실패
+	public boolean getMemberError(MemberVO memberVO, BindingResult bindingResult) throws Exception {
+		boolean check=false; //error가 없다, true : error가 있다. 검증실패
 		
 		//password 일치 검증
 		if(!memberVO.getPassword().equals(memberVO.getPasswordCheck())) {
-			check=true; // check=!check
+			check=true; //check=!check
 			
-			bindingResult.reject("passwordCheck","memberVO.password.equalCheck");
+			bindingResult.rejectValue("passwordCheck", "memberVO.password.equalCheck");
 		}
 		
-		//ID 중복 검사
-		MemberVO checkVO =memberDAO.getMember(memberVO);
+		// ID 중복 검사
+		MemberVO checkVO = memberDAO.getMember(memberVO);
 		
-		if(checkVO !=null) {
-			check=true;
-			bindingResult.reject("username", "memberVO.username.equalCheck");
+		if(checkVO != null) {
+			check = true;
+			bindingResult.rejectValue("username", "memberVO.username.equalCheck");
 		}
+		
 		
 		return check;
 	}
+	
+	@Transactional(rollbackFor = Exception.class)
+	public int setJoin (MemberVO memberVO)throws Exception{
+		memberVO.setPassword(passwordEncoder.encode(memberVO.getPassword()));
+		int result=memberDAO.setJoin(memberVO);
+		Map<String, Object> map=new HashMap<>();
+		map.put("roleNum", 3);
+		map.put("username", memberVO.getUsername());
+		result = memberDAO.setMemberRole(map);		
+		return result;
+	}
+
 }
